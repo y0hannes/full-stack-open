63cd870a35a8f166b0a3f82db1a1d4f421c4517d
y0hannes <yohanneset3@gmail.com>
2025-06-23 12:57:03 +0300

    Add Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      const notes = response.data
+      setPersons(notes)
+    })
+  }, [])
 
   const handleName = (e) => {
     setNewName(e.target.value)
@@ -53,7 +57,6 @@ const App = () => {
     setSearchQuery(e.target.value)
   }
 
-
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
@@ -72,11 +75,11 @@ const App = () => {
         nameChange={handleName}
         number={newNumber}
         numberChange={handleNumber}
-        />
+      />
 
       <h2>Numbers</h2>
-      <Persons persons={filteredPersons}/>
-      
+      <Persons persons={filteredPersons} />
+
     </div >
   )
 }
