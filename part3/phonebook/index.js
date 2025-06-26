de75e7659088ace61b843a647f1f6bb751e2437a
y0hannes <yohanneset3@gmail.com>
2025-06-26 22:04:19 +0300

    Add Exercises 3.1.-3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,13 +21,63 @@ persons = [
   }
 ]
 
-const url = "/api/persons"
-
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get(url, (request, response) => {
+const generateId = () => {
+  return Math.floor(Math.random() * 10000).toString()
+}
+
+app.get("/api/persons", (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const total = persons.length
+  const date = Date().toString()
+  response.send(`Phonebook has info for ${total} people.\n ${date}`)
+})
+
+app.get('/api/person/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id == id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/person/:id', (request, response) => {
+  const id = request.params.id
+  const toBeDeleted = persons.find(person => person.id == id)
+  if (toBeDeleted) {
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  }
+  else {
+    response.status(404).json({ error: 'Person not found' })
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const id = String(generateId())
+  const { name, number } = request.body
+
+  if (!name || !number) {
+    return response.status(400).json({ error: "name and number are required" })
+  }
+  const nameExist = persons.find(person => person.name === name)
+  if (nameExist){
+    return response.status(400).json({error:`${name} already exist`})
+  }
+
+  const person = { id, name, number }
+  persons.push(person)
+  response.status(201).json(person)
 })
 
 const PORT = 3001
