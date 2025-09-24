4d7114970e5942c8e91c7dbad21d5a42db8f102b
y0hannes <yohanneset3@gmail.com>
2025-09-24 14:19:31 +0300

    Improve schema validation using zod

--- part9/patientor/backend/src/routes/patients.ts
+++ part9/patientor/backend/src/routes/patients.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import patientService from '../services/patientService';
-import toNewPatientEntry from '../utils';
+import { newPatientEntry } from '../utils';
+import { z } from 'zod';
+import { NewPatientEntry, Patient } from '../types';
 
 const router = express.Router();
 
@@ -9,17 +11,40 @@ router.get('/', (_req, res) => {
   res.json(patients);
 });
 
-router.post('/', (req, res) => {
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
   try {
-    const newPatientEntity = toNewPatientEntry(req.body);
-    const addedEntity = patientService.addPatient(newPatientEntity);
-    res.json(addedEntity);
+    newPatientEntry.parse(req.body);
+    next();
   } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
+    next(error);
   }
-});
+};
+
+router.post(
+  '/',
+  newPatientParser,
+  (
+    req: Request<unknown, unknown, NewPatientEntry>,
+    res: Response<Patient>
+  ) => {
+    const addedEntity = patientService.addPatient(req.body);
+    res.json(addedEntity);
+  }
+);
+
+const errorHandler = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.use(errorHandler);
+
 export default router;
--- part9/patientor/backend/src/utils.ts
+++ part9/patientor/backend/src/utils.ts
@@ -1,77 +1,14 @@
 import { NewPatientEntry, Gender } from './types';
+import { z } from 'zod';
 
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
+export const newPatientEntry = z.object({
+  name: z.string(),
+  dateOfBirth: z.string().date(),
+  ssn: z.string(),
+  gender: z.enum(Gender),
+  occupation: z.string(),
+});
+
+export const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  return newPatientEntry.parse(object);
 };
-
-const parseName = (name: unknown): string => {
-  if (!name || !isString(name)) {
-    throw new Error('Incorrect or missing name ' + name);
-  }
-  return name;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDateOfBirth = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-    throw new Error('Incorrect or missing date ' + date);
-  }
-  return date;
-};
-
-const parseSsn = (ssn: unknown): string => {
-  if (!ssn || !isString(ssn)) {
-    throw new Error('Incorrect or missing ssn ' + ssn);
-  }
-  return ssn;
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(param);
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!gender || !isString(gender) || !isGender(gender)) {
-    throw new Error('Incorrect or missing gender ' + gender);
-  }
-  return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!occupation || !isString(occupation)) {
-    throw new Error('Incorrect or missing occupation ' + occupation);
-  }
-  return occupation;
-};
-
-const toNewPatientEntry = (object: unknown): NewPatientEntry => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
-  if (
-    'name' in object &&
-    'dateOfBirth' in object &&
-    'ssn' in object &&
-    'gender' in object &&
-    'occupation' in object
-  ) {
-    const newPatient: NewPatientEntry = {
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation),
-    };
-
-    return newPatient;
-  }
-
-  throw new Error('Incorrect data: some fields are missing');
-};
-
-export default toNewPatientEntry;
